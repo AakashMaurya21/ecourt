348497c6b8555e8ef5aecd284f4c48967ad570bd
aakashmaurya21 <aakashmaurya21@gmail.com>
2024-02-11 11:34:35 +0530

    second commit

--- signup.php
+++ signup.php
@@ -9,10 +9,11 @@ if (isset($_SESSION["user"])) {
 <html>
 
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Ecourt Login</title>
 </head>
 
@@ -24,6 +25,7 @@ if (isset($_SESSION["user"])) {
             if (isset($_POST["submit"])) {
                 $email = $_POST["email"];
                 $name = $_POST["name"];
+                $user = $_POST['user'];
                 $password = $_POST["password"];
                 $confirm_password = $_POST["confirm_password"];
                 $errors = array();
@@ -55,11 +57,11 @@ if (isset($_SESSION["user"])) {
                         echo "</div>";
                     }
                 } else {
-                    $sql = "INSERT INTO users (email, name, password) VALUES ( ? , ? , ?)";
+                    $sql = "INSERT INTO users (email, name,user, password) VALUES ( ? , ? , ?, ?)";
                     $stmt = mysqli_stmt_init($link);
                     $prep = mysqli_stmt_prepare($stmt, $sql);
                     if ($prep === true) {
-                        mysqli_stmt_bind_param($stmt, "sss", $email, $name, $password);
+                        mysqli_stmt_bind_param($stmt, "ssss", $email, $name, $user, $password);
                         mysqli_stmt_execute($stmt);
                         echo "<div class='alert alert-success'>You are Registered Successfully</div>";
                     }
@@ -76,6 +78,11 @@ if (isset($_SESSION["user"])) {
                 <form class="login-form" action="signup.php" method="post">
                     <input type="text" name="email" placeholder="Email" />
                     <input type="text" name="name" placeholder="Name" />
+                    <select name="user" id="user">
+                        <option value="Judge">Judge</option>
+                        <option value="Lawyer">Lawyer</option>
+                        <option value="Client">Client</option>
+                    </select>
                     <input type="password" name="password" placeholder="Password" />
                     <input type="password" name="confirm_password" placeholder="Confirm Password" />
                     <button type="submit" name="submit">Register</button>
